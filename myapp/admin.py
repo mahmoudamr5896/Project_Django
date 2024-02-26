6bf6e6a036f909f76ed2e742e25ca5a92071526d
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-02-26 18:12:32 +0000

    save

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from myapp.models import Category, CommentReport, Donation, Picture, Project, Comment, ProjectReport, Rating, Report 
+from myapp.models import Category, CommentReport, Donation, Picture, Project, Comment, ProjectReport, Rating, Report ,Tag
 
 
 # Register your models here.
@@ -13,6 +13,7 @@ admin.site.register(CommentReport)
 admin.site.register(Donation)
 admin.site.register(Comment)
 admin.site.register(Rating)
+admin.site.register(Tag)
 
 from django.contrib import admin
 
@@ -23,6 +24,5 @@ from myapp.models import Category, Comment, Donation, Picture, Project
 # admin.site.register(Category)
 # # admin.site.register(Project)
 # # admin.site.register(Picture)
-# # admin.site.register(Tag)
 # admin.site.register(Donation)
 # admin.site.register(Comment)
